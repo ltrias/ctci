f07564c5e23c0db24734c2135fdb8f66d3219bcc
ltrias <lucas.trias@gmail.com>
2017-06-03 10:47:20 -0300

    Creating intersection tests

--- src/ch2/intersect.js
+++ src/ch2/intersect.js
@@ -10,7 +10,7 @@ class Intersect{
     }
 
     findIntersection(){
-    
+        return {};
     }
 }
 
--- src/ch2/intersect.spec.js
+++ src/ch2/intersect.spec.js
@@ -19,7 +19,7 @@ describe('Intersect', () => {
     intersect = new Intersect(list1, list2);
   });
 
-  it.skip('No intersection', () => {
+  it('must not find intersection', () => {
     list1.appendToTail(1);
     list1.appendToTail(2);
     list1.appendToTail(3);
@@ -33,17 +33,78 @@ describe('Intersect', () => {
     result.should.be.null;
   });
 
-  it.skip('Intersection in the middle', () => {
-    let head1 = list1.head;
-    let head2 = list2.head;
+  it('must find intersection in the middle', () => {
+    let list3 = new LinkedList();
+    let intersectionNode = new Node(3);
+    list3.appendNodeToTail(intersectionNode);
+    list3.appendToTail(5);
+    list3.appendToTail(6);
 
+    list1.head = list3.head;
+    list1.appendToHead(2);
+    list1.appendToHead(1);
 
+    list2.head = list3.head;
+    list2.appendToHead(8);
+    list2.appendToHead(7);
+
+    let result = intersect.findIntersection();
+    result = result && intersectionNode == result;
+
+    result.should.be.true;
   });
 
-  it.skip('Intersection in the end', () => {
+  it('must find intersection in the tail', () => {
+    let list3 = new LinkedList();
+    let intersectionNode = new Node(3);
+    list3.appendNodeToTail(intersectionNode);
+
+    list1.head = list3.head;
+    list1.appendToHead(2);
+    list1.appendToHead(1);
+
+    list2.head = list3.head;
+    list2.appendToHead(8);
+    list2.appendToHead(7);
+
+    let result = intersect.findIntersection();
+    result = result && intersectionNode == result;
+
+    result.should.be.true;
   });
 
-  it.skip('Intersection in the head aka same list', () => {
+  it('must find intersection in the head(aka same list)', () => {
+    list1.appendToHead(2);
+    list1.appendToHead(1);
+    list1.appendToHead(1);
+
+    list2.head = list1.head;
+
+    let result = intersect.findIntersection();
+    result = result && list1.head == result;
+
+    result.should.be.true;
   });
 
+  it('must find intersection in the middle of different size lists', () => {
+    let list3 = new LinkedList();
+    let intersectionNode = new Node(3);
+    list3.appendNodeToTail(intersectionNode);
+    list3.appendToTail(5);
+    list3.appendToTail(6);
+
+    list1.head = list3.head;
+    list1.appendToHead(2);
+    list1.appendToHead(1);
+
+    list2.head = list3.head;
+    list2.appendToHead(9);
+    list2.appendToHead(8);
+    list2.appendToHead(7);
+
+    let result = intersect.findIntersection();
+    result = result && intersectionNode == result;
+
+    result.should.be.true;
+  });
 });
