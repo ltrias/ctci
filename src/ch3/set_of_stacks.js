59e439a7eed43980a811d3c30087e6103d47e6a0
ltrias <lucas.trias@gmail.com>
2017-06-10 14:25:21 -0300

    set of stacks

--- src/ch3/set_of_stacks.js
+++ src/ch3/set_of_stacks.js
@@ -5,29 +5,47 @@ let Stack = require('./stack').Stack;
 class SetOfStacks{
     constructor(threshold){
         this.threshold = threshold;
-        this.stacks = new Stack();
+        this.stacks = [];
+        this.lastStack = 0;
     }
 
     push(value){
-        let stack = this.stacks.peek();
+        let stack = this.stacks[this.lastStack];
 
-        if( stack == null || this.stackHeight(stack) == this.threshold ){
+        if( !stack ){
+            stack = new Stack();
+            this.stacks[this.lastStack] = stack;
+        }else if( this.stackHeight(stack) === this.threshold ){
             stack = new Stack();
             this.stacks.push(stack);
+            this.lastStack++;
         }
+
         stack.push(value);
     }
 
     pop(){
         let result = null;
-        let stack = this.stacks.peek();
+        let stack = this.stacks[this.lastStack];
 
         if( stack ){
             result = stack.pop();
-
+            
             if( stack.empty() ){
-                this.stacks.pop();
+                this.stacks = this.stacks.slice(0, this.lastStack);
+                this.lastStack--;
             }
+        }
+
+        return result;
+    }
+
+    popAt(_stack){
+        let result = null;
+        let stack = this.stacks[_stack];
+
+        if( stack ){
+            result = stack.pop();
         }
 
         return result;
--- src/ch3/set_of_stacks.spec.js
+++ src/ch3/set_of_stacks.spec.js
@@ -18,8 +18,45 @@ describe('Set of stacks', () => {
         }
         
         for(let i = 9; i >= 0; i--){
-            set.pop().should.be.equal(i);
+            let popped = set.pop();
+            popped.should.be.equal(i);
         }
         
+    });
+
+    it('popAt without insertion after', () => {
+        let set = new SetOfStacks(3);
+
+        for(let i = 0; i < 10; i++){
+            set.push(i);
+        }
+
+        set.popAt(1).should.be.equal(5);
+        
+        for(let i = 9; i >= 0; i--){
+            if( i === 5 ) continue;
+
+            let popped = set.pop();
+            popped.should.be.equal(i);
+        }
+    });
+
+    it('popAt with insertion after', () => {
+        let set = new SetOfStacks(2);
+        
+        set.push(0);
+        set.push(1);
+        set.push(2);
+        set.popAt(1);
+        set.push(3);
+        set.push(4);
+        set.push(5);
+
+        
+        for(let i = 5; i >= 0; i--){
+            if( i === 2 ) continue;
+            let popped = set.pop();
+            popped.should.be.equal(i);
+        }
     });
 });
